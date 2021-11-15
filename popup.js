40cab7efdca9d213962afb6c1962539370a9e3bd
Philipp Parzer <phil0898.pp@gmail.com>
2021-11-15 18:12:44 +0100

    added parsing

--- popup.js
+++ popup.js
@@ -12,10 +12,12 @@ startButton.addEventListener("click", async () => {
     //inject script into current tab
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: mainInject,
+      function: mainInject
     });
   });
   
+
+
   //injected script TODO: refactor into seperate module?
   function mainInject() {
       document.body.style.backgroundColor = "red";
@@ -23,16 +25,50 @@ startButton.addEventListener("click", async () => {
       
       //get selected text on page
     document.addEventListener('mouseup', event => {  
-      if (window.getSelection().toString().length !== 0) { // TODO: implement some regex tests e.g. if string only contains spaces
+      if (window.getSelection().toString().length !== 0) {
 
-        //check if string is only one word with space at end
-        if (/^[a-zA-Z]+\s$/.test(window.getSelection().toString())) {
+        //check if string is unicode word with one space at end or no space at end
+        if (window.getSelection().toString().match(/^[\u00C0-\u1FFF\u2C00-\uD7FF\w]+$/) || window.getSelection().toString().match(/^[\u00C0-\u1FFF\u2C00-\uD7FF\w]\s+$/) ) {
             let selectedText = window.getSelection().toString();
-            let selectedParentElement = window.getSelection().getRangeAt(0).startContainer.parentNode;
-            
-            selectedText = selectedText.substring(0, selectedText.length - 1);
-            console.log(selectedText.length);
             console.log(selectedText);
+            //get wiktionary data
+            fetch("https://en.wiktionary.org/wiki/" + selectedText) //TODO: CORS?
+            .then(response => response.text())
+            .then(html => {
+                console.log(html);
+
+                //parse string to get en definition
+                let defRegex = /\<ol\>\<li\>.*\<\/ol\>/;
+                let anchorDef = /\<a href=\"\/wiki\/[\u00C0-\u1FFF\u2C00-\uD7FF\w]+(\"|#).*/;
+
+                let regMatch = html.match(defRegex)
+                console.log("HTML MATCH " + regMatch)
+                let matchedAnchor = regMatch[0].match(anchorDef)[0];
+                console.log("Matched Anchor" + matchedAnchor);
+
+
+                let startIndex = matchedAnchor.indexOf('wiki\/') + 5;
+                let endIndex = startIndex;
+
+
+                for (let i = startIndex; i < matchedAnchor.length; i++) {
+                    if (matchedAnchor[i] === '"' || matchedAnchor[i] === '#') {
+                        endIndex = i;
+                        break;
+                    }
+                }
+
+                let wiktionaryDefResult = matchedAnchor.substring(startIndex, endIndex);
+                console.log("Result: " + wiktionaryDefResult);
+
+            })
+            .catch(error => console.error(error));
+
+
+
+            let selectedParentElement = window.getSelection().getRangeAt(0).startContainer.parentNode;            
+            //TODO: create a popover element at selectedParentElement
+
           }
       }
 
